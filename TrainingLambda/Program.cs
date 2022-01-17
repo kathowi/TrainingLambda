a984bf6179ef71807ba38fcbc8d9dc6f2a857c03
OBJECTIVITY\khowiszczak <khowiszczak@objectivity.co.uk>
2022-01-17 22:38:10 +0100

    adding LINQ actions

--- TrainingLambda/Program.cs
+++ TrainingLambda/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TrainingLambda
+﻿using System.Linq;
+
+namespace TrainingLambda
 {
     /*
      * More examples: 
@@ -15,10 +17,20 @@
             Animal animal1 = new Animal("Hela", 2, "dog");
             Animal animal2 = new Animal("Loki", 3, "cat");
             Animal animal3 = new Animal("Luna", 5, "cat");
-
+            Animal animal4 = new Animal("Pipi", 2, "hamster");
+            Animal animal5 = new Animal("Munia", 3, "dog");
+            Animal animal6 = new Animal("Łapek", 4, "cat");
+            Animal animal7 = new Animal("Spaky", 12, "dog");
+            Animal animal8 = new Animal("Alk", 2, "mouse");
+           
             animals.Add(animal1);
             animals.Add(animal2);
             animals.Add(animal3);
+            animals.Add(animal4);
+            animals.Add(animal5);
+            animals.Add(animal6);
+            animals.Add(animal7);
+            animals.Add(animal8);
 
             Console.WriteLine("Display all cats");
             var result = animals.Where(x => x.Type == "cat").ToList(); 
@@ -38,6 +50,25 @@
             var result1 = animals.Where(x => x.Type == input).ToList();
             result1.ForEach(x => x.PrintInfo());
 
+            Console.WriteLine("\nFirst animal in list");
+            animals.First().PrintInfo();
+
+            Console.WriteLine("\nAverage age of dog");
+            var dogs = animals.Where(x => x.Type == "dog");
+            Console.WriteLine(dogs.Average(y => y.Age));
+
+            Console.WriteLine("\nSort a-z by name");
+            var sortedAz = animals.OrderBy(x => x.Name).ToList();
+            foreach (var sorted in sortedAz)
+                Console.WriteLine(sorted.Name);
+
+            Console.WriteLine("\nDisplay all rodents");
+            //var rodents = animals.Where(x => x.Type == "mouse" || "hamster").ToList(); //not working
+            var mouses = animals.Where(x => x.Type == "mouse").ToArray();
+            var hamsters = animals.Where(x => x.Type == "hamster").ToArray();
+            var rodents = mouses.Concat(hamsters);
+            foreach (var rodent in rodents)
+                Console.WriteLine(rodent.Name);
         }
     }
 }
